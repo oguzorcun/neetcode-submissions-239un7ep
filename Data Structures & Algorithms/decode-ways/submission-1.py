cd3b6f9326864cd946efe79178c85bcf363c4bb9
Oğuz <oguzorcun@users.noreply.github.com>
2026-05-07 16:32:43 +0700

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        def decodes(code: str) -> bool:
+            return code[0] != '0' and 0 < int(code) < 27
+
+        @lru_cache(maxsize=None)
+        def rec(i) -> int:
+            if i == -1: return 1
+            elif i == 0:
+                return 1 if decodes(s[0]) else 0
+
+            single = double = 0
+            if decodes(s[i]): single = rec(i-1)
+            if decodes(s[i-1:i+1]): double = rec(i-2)
+
+            return single + double
+
+        return rec(len(s) - 1)        
+            
+
